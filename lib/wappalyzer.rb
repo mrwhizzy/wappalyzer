02055cd6f9b1dcc7d90d8ca10c75e0c65f4273ae
Christos Melas <cmelas@skroutz.gr>
2020-12-09 21:47:53 +0200

    Add logic to follow redirects and refactor Wappalyzer::Detector

--- lib/wappalyzer.rb
+++ lib/wappalyzer.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'wappalyzer/version'
-
-require 'net/http'
-require 'mini_racer'
 require 'json'
+require 'mini_racer'
+require 'net/http'
 require 'zlib'
 
 Encoding.default_external = Encoding::UTF_8
@@ -13,37 +11,41 @@ Encoding.default_external = Encoding::UTF_8
 module Wappalyzer
   # Detector class
   class Detector
-    def initialize
-      @realdir = __dir__
-      file = File.join(@realdir, 'apps.json')
-      @json = JSON.parse(IO.read(file))
-      @categories = @json['categories']
-      @apps = @json['apps']
-    end
+    class TooManyRedirectsError < StandardError; end
 
-    def analyze(url)
+    IF_MODIFIED_TIME = Time.now - 60 * 60
+
+    REALDIR = __dir__
+    JSON_FILE = JSON.parse(IO.read(File.join(REALDIR, 'apps.json')))
+    CATEGORIES = JSON_FILE['categories'].to_json
+    APPS = JSON_FILE['apps'].to_json
+
+    def analyze(url, redirect_limit = 10, cookies = nil)
+      raise TooManyRedirectsError.new('Too many HTTP redirects') if redirect_limit.zero?
+
       uri = URI(url)
-      body = nil
-      headers = {}
-      Net::HTTP.start(uri.host, uri.port,
-                      use_ssl: uri.scheme == 'https',
-                      verify_mode: OpenSSL::SSL::VERIFY_NONE,
-                      open_timeout: 5) do |http|
-        begin
-          resp = http.get(uri.request_uri)
-        rescue Zlib::DataError
-          resp = http.get(uri.request_uri, 'Accept-Encoding' => 'none')
-        end
+      req = build_request(uri, cookies)
+      conf = { use_ssl: uri.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE, open_timeout: 5 }
 
-        resp.each_header { |k, v| headers[utf8_encoding(k).downcase] = utf8_encoding(v) }
-        body = utf8_encoding(resp.body)
+      res = Net::HTTP.start(uri.hostname, uri.port, conf) do |http|
+        http.request(req)
+      rescue Zlib::DataError
+        req['Accept-Encoding'] = 'none'
+        http.request(req)
       end
 
+      return analyze(URI.join(uri, res['location']), redirect_limit - 1,
+                     res['Set-Cookie']) if res.is_a? Net::HTTPRedirection
+
+      headers = res.each_header.each_with_object({}) { |(k, v), hsh| hsh[utf8_encoding(k).downcase] = utf8_encoding(v) }
+      body = utf8_encoding(res.body)
+
       cxt = MiniRacer::Context.new
-      cxt.load File.join(@realdir, 'js', 'wappalyzer.js')
-      cxt.load File.join(@realdir, 'js', 'driver.js')
+      cxt.load(File.join(REALDIR, 'js', 'wappalyzer.js'))
+      cxt.load(File.join(REALDIR, 'js', 'driver.js'))
       data = { 'host' => uri.hostname, 'url' => url, 'html' => body, 'headers' => headers }
-      output = cxt.eval("w.apps = #{@apps.to_json}; w.categories = #{@categories.to_json}; w.driver.data = #{data.to_json}; w.driver.init();")
+      output = cxt.eval("w.apps = #{APPS}; w.categories = #{CATEGORIES}; " \
+                        "w.driver.data = #{data.to_json}; w.driver.init();")
       JSON.parse(output)
     end
 
@@ -52,14 +54,25 @@ module Wappalyzer
     def utf8_encoding(str)
       str.encode('UTF-8', invalid: :replace, undef: :replace)
     end
+
+    def build_request(uri, cookies = nil)
+      req = Net::HTTP::Get.new(uri)
+
+      req['Host'] = uri.host
+      req['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36' \
+        '(KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
+      req['Referer'] = 'https://www.google.com/'
+      req['Set-Cookie'] = cookies
+
+      req
+    end
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  url = ARGV[0]
-  if url
-    puts JSON.pretty_generate(Wappalyzer::Detector.new.analyze(ARGV[0]))
+  if ARGV[0]
+    puts JSON.pretty_generate(Wappalyzer::Detector.new.analyze(ARGV[0], ARGV[1].to_i))
   else
-    puts "Usage: #{__FILE__} http://example.com"
+    puts "Usage: #{__FILE__} http://example.com [redirects_limit]"
   end
 end
